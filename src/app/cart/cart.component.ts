26009d2480354cb61168d9eb73125be593458ae0
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-12-19 10:16:39 -0600

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,12 +8,21 @@ import { CartService } from '../services/cart.service';
 })
 export class CartComponent implements OnInit {
 
+  // items = [[["Original", "Medium", "Normal", "BBQ"], ["Normal", "Parmesan", "cheddar"], ["Sausage", "beef"], ["tomatoes", "spinach"]]];
+
   items = [];
+  cheeseToppings = [];
+  meatToppings = [];
+  veggiesToppings = [];
 
   constructor(private cartService: CartService) { }
 
   ngOnInit() { 
     this.items = this.cartService.getItems();
-    console.log(this.items);
+    if(this.items[0] && this.items[0].length > 1) {
+      this.cheeseToppings = this.items[0][1].slice(1, this.items[0][1].length);
+      this.meatToppings = this.items[0][2];
+      this.veggiesToppings = this.items[0][3];
+    }
   } 
 }
